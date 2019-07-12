d9fd931cd2d35ad636459aada8636f7ca73c2374
IntegratedQuantum <jahe788@gmail.com>
2019-07-12 16:50:12 +0200

    Use a more object orientated approach for the positioning system(using Vectors). Also use a List-like behaviour to increase the seg array in Snake.

--- src/Object.java
+++ src/Object.java
@@ -4,7 +4,7 @@ import java.util.List;
 import javax.swing.JFrame;
 
 public class Object extends JFrame {
-	int [] pos = new int [3];
+	Vector pos = new Vector();
 	boolean eatable;
 	Color color;
 
@@ -12,30 +12,30 @@ public class Object extends JFrame {
 	public Object(int size, Snake snake, List<Object> o) {
 		eatable = true;
 		do {
-			pos[0] = (int)(Math.random()*size);
-			pos[1] = (int)(Math.random()*size);
-			pos[2] = (int)(Math.random()*5);
+			pos.x = (int)(Math.random()*size);
+			pos.y = (int)(Math.random()*size);
+			pos.z = (int)(Math.random()*5);
 		}while(onSnake(snake) || onBlock(o)); // Don't put on object on another object/on the snake.
 		// Tell g3d to paint the object with a random color(not matching background or border color).
-		Assets.g3d.setColor(pos[0], pos[1], pos[2], new Color(55+(int)(Math.random()*150), 55+(int)(Math.random()*150), 55+(int)(Math.random()*150)));
+		Assets.g3d.setColor(pos.x, pos.y, pos.z, new Color(55+(int)(Math.random()*150), 55+(int)(Math.random()*150), 55+(int)(Math.random()*150)));
 	}
 	
 	// Create an object at a certain position. Only used for level creation → no check if onSnake/onBlock.
 	public Object(int x, int y, int l) {
-		pos[0] = x;
-		pos[1] = y;
-		pos[2] = l;
+		pos.x = x;
+		pos.y = y;
+		pos.z = l;
 		color = new Color(250, 250, 250); // color for borders.
 		eatable = false;
 		// Tell g3d to paint the object.
-		Assets.g3d.setColor(pos[0], pos[1], pos[2], color);
+		Assets.g3d.setColor(pos.x, pos.y, pos.z, color);
 	}
 	
 	// Determines if this object shares the location with an object from the list.
 	public boolean onBlock(List<Object> o) {
 		boolean on = false;
 		for(int i = 0; i < o.size(); i++) {
-			if(pos[0] == o.get(i).pos[0] && pos[1] == o.get(i).pos[1] && pos[2] == o.get(i).pos[2]) {
+			if(pos.x == o.get(i).pos.x && pos.y == o.get(i).pos.y && pos.z == o.get(i).pos.z) {
 				on = true;
 			}
 		}
@@ -46,7 +46,7 @@ public class Object extends JFrame {
 	public boolean onSnake(Snake snake) {
 		boolean on = false;
 		for(int i = snake.length; i >= 0; i--) {
-			if(snake.seg[i][0] == pos[0] && snake.seg[i][1] == pos[1]) {
+			if(snake.seg[i].x == pos.x && snake.seg[i].y == pos.y) {
 				on = true;
 			}
 		}
@@ -55,7 +55,7 @@ public class Object extends JFrame {
 	
 	// Checks if the object is on the location specified by x,y,l.
 	public boolean isEaten(int x, int y, int l) {
-		if(x == pos[0] && y == pos[1] && l == pos[2]) {
+		if(x == pos.x && y == pos.y && l == pos.z) {
 			return true;
 		}
 		return false;
@@ -66,7 +66,7 @@ public class Object extends JFrame {
 		int [] opposite = {2, 3, 0, 1, 5, 4};
 		eatable = false;
 		color = new Color(250, 250, 250); // color for borders.
-		pos[2] = opposite[pos[2]];
-		Assets.g3d.setColor(pos[0], pos[1], pos[2], color);
+		pos.z = opposite[pos.z];
+		Assets.g3d.setColor(pos.x, pos.y, pos.z, color);
 	}
 }
--- src/Snake.java
+++ src/Snake.java
@@ -2,7 +2,7 @@ import java.awt.Color;
 import java.util.List;
 
 public class Snake {
-	int [][] seg = new int [50][3]; // I don't expect you to get a higher score than 50. TODO: Allow higher scores without crashing.
+	Vector [] seg = new Vector [8]; // Uses the same functionality as a List(see Snake.growArray()), but can be handled like an array.
 	int length = 3; // Start with a length of 3.
 	Color color = new Color(0, 255, 0); // TODO: make this changeable from a menu.
 	private int size;
@@ -15,15 +15,13 @@ public class Snake {
 	private int dir;
 
 	public Snake(int size, int x, int y, int l) {
-		for(int i = 0; i < 50; i++) {
-			for(int j = 0; j < 3; j++) {
-				seg[i][j] = 1000; // Give non-existent segments an arbitrary high default value.
-			}
-		}
 		this.size = size-1; // Size of the cube.
-		seg[0][0] = x;
-		seg[0][1] = y;
-		seg[0][2] = l; // l is a value between 0 and 5 representing the face this segment is on.
+		for(int i = 0; i < seg.length; i++) {
+			seg[i] = new Vector(1000, 1000, 1000);// Give not-yet-existent segments an arbitrary high default value.
+		}
+		seg[0].x = x;
+		seg[0].y = y;
+		seg[0].z = l; // l is a value between 0 and 5 representing the face this segment is on.
 		dir = 0;
 	}
 	
@@ -42,137 +40,140 @@ public class Snake {
 	public void move() {
 		try {
 			// Remove the end of the tail from the list to paint by g3d.
-			Assets.g3d.setColor(seg[length][0], seg[length][1], seg[length][2], Color.BLACK);
+			Assets.g3d.setColor(seg[length].x, seg[length].y, seg[length].z, Color.BLACK);
 		}catch(Exception ignored){}
 		// Moving the tail.
 		for(int i = length; i > 0; i--) {
-			seg[i][0] = seg[i-1][0];
-			seg[i][1] = seg[i-1][1];
-			seg[i][2] = seg[i-1][2];
+			seg[i].x = seg[i-1].x;
+			seg[i].y = seg[i-1].y;
+			seg[i].z = seg[i-1].z;
 		}
 		// Moving the head.
-		seg[0][0] += diruse[dir][0];
-		seg[0][1] += diruse[dir][1];
+		seg[0].x += diruse[dir][0];
+		seg[0].y += diruse[dir][1];
 		// Determines to which face the head goes next if it changes the face.
 		// TODO: Simplify this and make it easier to understand.
-		if(seg[0][2] < 4) {
-			if(seg[0][1] < 0) {
-				seg[0][1] = size;
-				seg[0][2]--;
-				if(seg[0][2] < 0) {
-					seg[0][2] = 3;
+		if(seg[0].z < 4) {
+			if(seg[0].y < 0) {
+				seg[0].y = size;
+				seg[0].z--;
+				if(seg[0].z < 0) {
+					seg[0].z = 3;
 				}
 			}
-			else if(seg[0][1] > size) {
-				seg[0][1] = 0;
-				seg[0][2]++;
-				if(seg[0][2] > 3) {
-					seg[0][2] = 0;
+			else if(seg[0].y > size) {
+				seg[0].y = 0;
+				seg[0].z++;
+				if(seg[0].z > 3) {
+					seg[0].z = 0;
 				}
 			}
-			if(seg[0][0] > size) {
-				switch(seg[0][2]) {
+			if(seg[0].x > size) {
+				switch(seg[0].z) {
 				case 0:
-					seg[0][0] = size;
-					seg[0][1] = size-seg[0][1];
+					seg[0].x = size;
+					seg[0].y = size-seg[0].y;
 					dir = 3;
 					break;
 				case 1:
-					seg[0][0] = size-seg[0][1];
-					seg[0][1] = 0;
+					seg[0].x = size-seg[0].y;
+					seg[0].y = 0;
 					dir = 2;
 					break;
 				case 2:
-					seg[0][0] = 0;
+					seg[0].x = 0;
 					break;
 				case 3:
-					seg[0][0] = seg[0][1];
-					seg[0][1] = size;
+					seg[0].x = seg[0].y;
+					seg[0].y = size;
 					dir = 0;
 				}
-				seg[0][2] = 4;
+				seg[0].z = 4;
 			}
-			else if(seg[0][0] < 0) {
-				switch(seg[0][2]) {
+			else if(seg[0].x < 0) {
+				switch(seg[0].z) {
 				case 0:
-					seg[0][0] = size;
-					seg[0][1] = seg[0][1];
+					seg[0].x = size;
+					seg[0].y = seg[0].y;
 					dir = 3;
 					break;
 				case 1:
-					seg[0][0] = size-seg[0][1];
-					seg[0][1] = size;
+					seg[0].x = size-seg[0].y;
+					seg[0].y = size;
 					dir = 0;
 					break;
 				case 2:
-					seg[0][0] = 0;
-					seg[0][1] = size-seg[0][1];
+					seg[0].x = 0;
+					seg[0].y = size-seg[0].y;
 					dir = 1;
 					break;
 				case 3:
-					seg[0][0] = seg[0][1];
-					seg[0][1] = 0;
+					seg[0].x = seg[0].y;
+					seg[0].y = 0;
 					dir = 2;
 				}
-				seg[0][2] = 5;
+				seg[0].z = 5;
 			}
 		}
-		else if(seg[0][2] == 4) {
-			if(seg[0][0] < 0) {
-				seg[0][2] = 2;
-				seg[0][0] = size;
+		else if(seg[0].z == 4) {
+			if(seg[0].x < 0) {
+				seg[0].z = 2;
+				seg[0].x = size;
 			}
-			else if(seg[0][0] > size) {
-				seg[0][2] = 0;
-				seg[0][0] = size;
-				seg[0][1] = size-seg[0][1];
+			else if(seg[0].x > size) {
+				seg[0].z = 0;
+				seg[0].x = size;
+				seg[0].y = size-seg[0].y;
 				dir = 3;
 			}
-			if(seg[0][1] < 0) {
-				seg[0][2] = 1;
-				seg[0][1] = size-seg[0][0];
-				seg[0][0] = size;
+			if(seg[0].y < 0) {
+				seg[0].z = 1;
+				seg[0].y = size-seg[0].x;
+				seg[0].x = size;
 				dir = 3;
 			}
-			else if(seg[0][1] > size) {
-				seg[0][2] = 3;
-				seg[0][1] = seg[0][0];
-				seg[0][0] = size;
+			else if(seg[0].y > size) {
+				seg[0].z = 3;
+				seg[0].y = seg[0].x;
+				seg[0].x = size;
 				dir = 3;
 			}
 		}
 		else {
-			if(seg[0][0] < 0) {
-				seg[0][2] = 2;
-				seg[0][0] = 0;
-				seg[0][1] = size-seg[0][1];
+			if(seg[0].x < 0) {
+				seg[0].z = 2;
+				seg[0].x = 0;
+				seg[0].y = size-seg[0].y;
 				dir = 1;
 			}
-			else if(seg[0][0] > size) {
-				seg[0][2] = 0;
-				seg[0][0] = 0;
+			else if(seg[0].x > size) {
+				seg[0].z = 0;
+				seg[0].x = 0;
 			}
-			if(seg[0][1] < 0) {
-				seg[0][2] = 3;
-				seg[0][1] = seg[0][0];
-				seg[0][0] = 0;
+			if(seg[0].y < 0) {
+				seg[0].z = 3;
+				seg[0].y = seg[0].x;
+				seg[0].x = 0;
 				dir = 1;
 			}
-			else if(seg[0][1] > size) {
-				seg[0][2] = 1;
-				seg[0][1] = size-seg[0][0];
-				seg[0][0] = 0;
+			else if(seg[0].y > size) {
+				seg[0].z = 1;
+				seg[0].y = size-seg[0].x;
+				seg[0].x = 0;
 				dir = 1;
 			}
 		}
 		// Tell g3d where to paint the new head.
-		Assets.g3d.setColor(seg[0][0], seg[0][1], seg[0][2], Color.GREEN);
+		Assets.g3d.setColor(seg[0].x, seg[0].y, seg[0].z, Color.GREEN);
 	}
 	
 	// Determines if the object is eaten.
 	public boolean eat(Object o) {
-		if(o.isEaten(seg[0][0], seg[0][1], seg[0][2])) {
+		if(o.isEaten(seg[0].x, seg[0].y, seg[0].z)) {
 			length++;
+			if(length == seg.length) {
+				growArray();
+			}
 			return true;
 		}
 		return false;
@@ -181,7 +182,7 @@ public class Snake {
 	// Determines if the snake bites its own tail.
 	public boolean eatSelf() {
 		for(int i = 1; i < length; i++) {
-			if(seg[0][0] == seg[i][0] && seg[0][1] == seg[i][1] && seg[0][2] == seg[i][2]) {
+			if(seg[0].x == seg[i].x && seg[0].y == seg[i].y && seg[0].z == seg[i].z) {
 				return true;
 			}
 		}
@@ -191,10 +192,21 @@ public class Snake {
 	// Determines if an object from the list(currently only used by borders) is eaten.
 	public boolean eatBorder(List<Object> o) {
 		for(int i = 0; i < o.size(); i++) {
-			if(o.get(i).isEaten(seg[0][0], seg[0][1], seg[0][2])) {
+			if(o.get(i).isEaten(seg[0].x, seg[0].y, seg[0].z)) {
 				return true;
 			}
 		}
 		return false;
+	}
+
+	// Increases the size of the seg array by the factor 2 and fills the empty places with new Vectors of arbitrary high value.
+	// Decreasing isn't necessary, because Snake gets newly instantiated on the start of each game.
+	public void growArray() {
+		Vector[] old = seg;
+		seg = new Vector[old.length*2];
+		System.arraycopy(old, 0, seg, 0, old.length);
+		for(int i = old.length; i < seg.length; i++) {
+			seg[i] = new Vector(1000, 1000, 1000);
+		}
 	}
 }
